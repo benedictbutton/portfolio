96fa2fcdd8537b6ed557a19e5ec8db3a76ffef47
benedictbutton <benedictdaly@hotmail.com>
2019-06-10 01:10:09 -0400

    reresh token

--- src/server/routes.js
+++ src/server/routes.js
@@ -17,8 +17,6 @@ router.post("/send", async (req, res) => {
           user: process.env.USER,
           clientId: process.env.CLIENT_ID,
           clientSecret: process.env.CLIENT_SECRET,
-          accessToken:
-            "ya29.GlskB6Dh5lYyJa7q7I1ADidXEtCgyOi61khd_i1lnfTWLgCpRmGXhpFsBVi0XzvP1YF2e9XSO0_9KeErXKEhyuRCdkfzrd25pztJ18mTj0wCHI8rT1ZL_-LPnPk-",
           refreshToken: "1/ZgOMbc2exLXtMEAX3mOFtcC61ThD3LVtPOnZ_Ix_NQc"
         })
       }
