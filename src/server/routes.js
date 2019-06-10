cc49714669a9daeef38e97b59644e3d17508c37d
benedictbutton <benedictdaly@hotmail.com>
2019-06-09 23:41:25 -0400

    sytax error

--- src/server/routes.js
+++ src/server/routes.js
@@ -8,15 +8,16 @@ const router = express.Router();
 router.post("/send", async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: "smtp.gmail.com",
       port: 465,
       secure: true,
       auth: {
         type: "OAuth2",
-        user: 'benedictdaly@gmail.com',
+        user: "benedictdaly@gmail.com",
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        accessToken: 'ya29.GlskB6Dh5lYyJa7q7I1ADidXEtCgyOi61khd_i1lnfTWLgCpRmGXhpFsBVi0XzvP1YF2e9XSO0_9KeErXKEhyuRCdkfzrd25pztJ18mTj0wCHI8rT1ZL_-LPnPk-'
+        accessToken:
+          "ya29.GlskB6Dh5lYyJa7q7I1ADidXEtCgyOi61khd_i1lnfTWLgCpRmGXhpFsBVi0XzvP1YF2e9XSO0_9KeErXKEhyuRCdkfzrd25pztJ18mTj0wCHI8rT1ZL_-LPnPk-",
         refreshToken: "1/ZgOMbc2exLXtMEAX3mOFtcC61ThD3LVtPOnZ_Ix_NQc"
       }
     });
