7eab8b6ba6741a69d278067e2f89ec60f3c0427e
benedictbutton <benedictdaly@hotmail.com>
2019-06-10 00:56:52 -0400

    xoauth2

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const rp = require("request-promise");
 const nodemailer = require("nodemailer");
+const xoauth2 = require("xoauth2");
 
 const router = express.Router();
 
@@ -12,13 +13,22 @@ router.post("/send", async (req, res) => {
       port: 465,
       secure: true,
       auth: {
-        type: "OAuth2",
-        user: process.env.USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        accessToken:
-          "ya29.GlskB6Dh5lYyJa7q7I1ADidXEtCgyOi61khd_i1lnfTWLgCpRmGXhpFsBVi0XzvP1YF2e9XSO0_9KeErXKEhyuRCdkfzrd25pztJ18mTj0wCHI8rT1ZL_-LPnPk-",
-        refreshToken: "1/ZgOMbc2exLXtMEAX3mOFtcC61ThD3LVtPOnZ_Ix_NQc"
+        xoauth2: xoauth2.createXOAuth2Generator({
+          user: process.env.USER,
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          accessToken:
+            "ya29.GlskB6Dh5lYyJa7q7I1ADidXEtCgyOi61khd_i1lnfTWLgCpRmGXhpFsBVi0XzvP1YF2e9XSO0_9KeErXKEhyuRCdkfzrd25pztJ18mTj0wCHI8rT1ZL_-LPnPk-",
+          refreshToken: "1/ZgOMbc2exLXtMEAX3mOFtcC61ThD3LVtPOnZ_Ix_NQc"
+        })
+      }
+    });
+
+    transporter.verify(function(error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
       }
     });
 
