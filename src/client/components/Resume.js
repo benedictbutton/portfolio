83d691280a7da1b9fae4561b69d46c805ffbfc75
benedictbutton <benedictdaly@hotmail.com>
2019-06-10 08:00:04 -0400

    again

--- src/client/components/Resume.js
+++ src/client/components/Resume.js
@@ -1,11 +1,16 @@
 import React from "react";
+import resume from "../Resume.pdf";
 
 const Resume = () => {
   return (
     <div className="container">
       <h2>Resume</h2>
       <div className="embed-responsive embed-responsive-4by3">
-        <object data="./src/client/Resume.pdf" type="application/pdf" />
+        <iframe
+          className="embed-responsive-item"
+          src={resume}
+          type="application/pdf"
+        />
       </div>
     </div>
   );
