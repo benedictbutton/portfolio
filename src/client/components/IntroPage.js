272e76801c797966d6c59cdb045dd25715df0618
benedictbutton <benedictdaly@hotmail.com>
2019-06-17 05:03:03 -0400

    profile pic

--- src/client/components/IntroPage.js
+++ src/client/components/IntroPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import image from "../assets/profile-pic.jpeg";
 
 const IntroPage = () => {
   return (
     <div className="home d-flex flex-grow-1">
       <div className="d-flex flex-grow-1 flex-column align-items-center titles">
+        <div className="img-responsive">
+          <img
+            src={image}
+            className="img-circle border border-primary border-3"
+          />
+        </div>
         <h1 className="title text-center">Benedict Daly</h1>
         <h3 className="subtitle text-center">
           Jr Full Stack Software Developer
