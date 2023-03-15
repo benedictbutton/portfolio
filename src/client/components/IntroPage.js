0c0b2c82793e119918a6c610a62551be025695d3
benedict button <benedictdaly@hotmail.com>
2023-03-15 18:58:08 -0400

    larger intro image and blinking arrow

--- src/client/components/IntroPage.js
+++ src/client/components/IntroPage.js
@@ -1,34 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import image from "../assets/profile-pic-1.jpg";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import image from '../assets/profile-pic-1.jpg'
 
 const IntroPage = () => {
-  return (
-    <div className="home d-flex flex-grow-1">
-      <div className="d-flex flex-grow-1 flex-column align-items-center titles">
-        <div className="img-responsive">
-          <img
-            src={image}
-            className="img-circle border border-primary border-3 h-100"
-          />
+    const position = '{ Software Engineer }'
+    return (
+        <div className="home d-flex flex-grow-1">
+            <div className="d-flex flex-grow-1 flex-column align-items-center titles">
+                <div className="img-responsive intro">
+                    <img
+                        src={image}
+                        className="img-circle border border-primary border-3 h-100"
+                    />
+                </div>
+                <h1 className="title text-center">Benedict Daly</h1>
+                <h3 className="subtitle text-center">{position}</h3>
+                <div>
+                    <Link to="/home" className="blink">
+                        <FontAwesomeIcon
+                            className="icon"
+                            icon={['far', 'arrow-alt-circle-right']}
+                            size="2x"
+                        />
+                    </Link>
+                </div>
+            </div>
         </div>
-        <h1 className="title text-center">Benedict Daly</h1>
-        <h3 className="subtitle text-center">
-        Software Engineer
-        </h3>
-        <div>
-          <Link to="/home">
-            <FontAwesomeIcon
-              className="icon"
-              icon={["far", "arrow-alt-circle-right"]}
-              size="2x"
-            />
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default IntroPage;
+export default IntroPage
