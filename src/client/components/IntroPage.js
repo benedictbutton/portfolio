c813848f8d21058474acd02adb6f90f752bbe5d1
benedict button <benedictdaly@hotmail.com>
2023-03-23 17:53:17 -0400

    changes intro page background

--- src/client/components/IntroPage.js
+++ src/client/components/IntroPage.js
@@ -6,24 +6,36 @@ import image from '../assets/profile-pic-1.jpg'
 const IntroPage = () => {
     const position = '{ Software Engineer }'
     return (
-        <div className="home d-flex flex-grow-1">
+        <div className="home">
             <div className="d-flex flex-grow-1 flex-column align-items-center titles">
-                <div className="img-responsive intro">
+                {/* <div className="img-responsive intro">
                     <img
                         src={image}
                         className="img-circle border border-primary border-3 h-100"
                     />
-                </div>
-                <h1 className="title text-center">Benedict Daly</h1>
-                <h3 className="subtitle text-center">{position}</h3>
-                <div>
-                    <Link to="/home" className="blink">
-                        <FontAwesomeIcon
-                            className="icon"
-                            icon={['far', 'arrow-alt-circle-right']}
-                            size="2x"
-                        />
-                    </Link>
+                </div> */}
+                <div
+                    style={{
+                        borderColor: 'white',
+                        borderStyle: 'solid',
+                        borderWidth: 'thick',
+                        backgroundColor: 'rgba(40,67,135, .6)',
+                        zIndex: 1000,
+                        transform: 'translateY(50%)',
+                        padding: '1.2rem',
+                    }}
+                >
+                    <h1 className="title text-center">Benedict Daly</h1>
+                    <h3 className="subtitle text-center">{position}</h3>
+                    <div style={{ textAlign: 'center' }}>
+                        <Link to="/home" className="blink">
+                            <FontAwesomeIcon
+                                className="icon"
+                                icon={['far', 'arrow-alt-circle-right']}
+                                size="2x"
+                            />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
