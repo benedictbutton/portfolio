e9b3be7897544a9e8a8d6e81231d32fb0246d096
benedict button <benedictdaly@hotmail.com>
2023-03-30 13:05:07 -0400

    new resume and updates to work experience

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,45 +1,46 @@
-"use strict";
-const express = require("express");
-const rp = require("request-promise");
-const nodemailer = require("nodemailer");
+'use strict'
+const express = require('express')
+const rp = require('request-promise')
+const nodemailer = require('nodemailer')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/send", async (req, res) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        type: "OAuth2",
-        user: process.env.USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN
-      }
-    });
+router.post('/send', async (req, res) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user: process.env.USER,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                // accessToken: accessToken,
+            },
+        })
 
-    transporter.verify(function(error, success) {
-      if (error) throw new Error(error);
-      else console.log("Server is ready to take our messages");
-    });
+        transporter.verify(function (error, success) {
+            if (error) throw 'New exception'
+            else console.log('Server is ready to take our messages')
+        })
 
-    const mailOptions = {
-      from: process.env.USER,
-      to: "benedictdaly@hotmail.com",
-      text: `${req.body.message}`,
-      replyTo: `${req.body.name} <${req.body.email}>`
-    };
+        const mailOptions = {
+            from: process.env.USER,
+            to: 'benedictdaly@hotmail.com',
+            text: `${req.body.message}`,
+            replyTo: `${req.body.name} <${req.body.email}>`,
+        }
 
-    transporter.sendMail(mailOptions, function(err, res) {
-      if (err) throw new Error(err);
-      else console.log("here is the res: ", res);
-    });
-    res.status(200).json({ message: "Email successfully sent" });
-  } catch (err) {
-    res.status(err.statusCode || 502).json(err.error || err);
-  }
-});
+        transporter.sendMail(mailOptions, function (err, res) {
+            if (err) throw new Error(err)
+            else console.log('here is the res: ', res)
+        })
+        res.status(200).json({ message: 'Email successfully sent' })
+    } catch (err) {
+        res.status(err.statusCode || 502).json(err.error || err)
+    }
+})
 
-module.exports = router;
+module.exports = router
